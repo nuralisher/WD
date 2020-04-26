0984a93b2a454b2d75db133d91e7f83ea0bdeb0c
nuralisher <alisher110301gmail.com>
2020-04-26 23:04:00 +0600

    project

--- week13(lab10)/django/hhBack/api/urls.py
+++ week13(lab10)/django/hhBack/api/urls.py
@@ -4,24 +4,24 @@ from rest_framework_jwt.views import obtain_jwt_token
 from api.views.views import company_list, company_detail, vacancies_with_company, vacancy_detail, vacancy_list, top_ten_vacancies
 from api.views.views_cbv import CompanyList, CompanyDetail, VacancyList, VacancyDetail, TopVacancyList, VacancyWithCompany
 
-# urlpatterns = [
-#     path('login', obtain_jwt_token),
-#
-#     path('companies/', company_list),
-#     path('companies/<int:pk>', company_detail),
-#     path('companies/<int:pk>/vacancies', vacancies_with_company),
-#     path('vacancies', vacancy_list),
-#     path('vacancies/<int:pk>', vacancy_detail),
-#     path('vacancies/top_ten', top_ten_vacancies),
-# ]
-
 urlpatterns = [
-path('login', obtain_jwt_token),
+    path('login', obtain_jwt_token),
 
-    path('companies/', CompanyList.as_view()),
-    path('companies/<int:pk>', CompanyDetail.as_view()),
-    path('companies/<int:pk>/vacancies', VacancyWithCompany.as_view()),
-    path('vacancies', VacancyList.as_view()),
-    path('vacancies/<int:pk>', VacancyDetail.as_view()),
-    path('vacancies/top_ten', TopVacancyList.as_view()),
+    path('companies/', company_list),
+    path('companies/<int:pk>', company_detail),
+    path('companies/<int:pk>/vacancies', vacancies_with_company),
+    path('vacancies', vacancy_list),
+    path('vacancies/<int:pk>', vacancy_detail),
+    path('vacancies/top_ten', top_ten_vacancies),
 ]
+
+# urlpatterns = [
+# path('login', obtain_jwt_token),
+#
+#     path('companies/', CompanyList.as_view()),
+#     path('companies/<int:pk>', CompanyDetail.as_view()),
+#     path('companies/<int:pk>/vacancies', VacancyWithCompany.as_view()),
+#     path('vacancies', VacancyList.as_view()),
+#     path('vacancies/<int:pk>', VacancyDetail.as_view()),
+#     path('vacancies/top_ten', TopVacancyList.as_view()),
+# ]
